a25f8210fdab6ec8bd56674b97ffcaa62ac3f8ab
elizaveta.larina <eliz.lar@yandex.ru>
2023-03-21 15:38:55 +0100

    update

--- 4_Hardhat/scripts/1_hardhat.js
+++ 4_Hardhat/scripts/1_hardhat.js
@@ -19,9 +19,9 @@ const ethers = require("ethers");
 // blockchain.
 
 // Your code here!
-// console.log(ethers.version)
+console.log(ethers.version)
 const network = "http://127.0.0.1:8545";
-const hardhatProvider = new ethers.JsonRpcProvider(network);
+const hardhatProvider = new ethers.JsonRpcProvider(network); //???
 
 // Exercise 2. Let's query the provider.
 ////////////////////////////////////////
@@ -32,14 +32,9 @@ const hardhatProvider = new ethers.JsonRpcProvider(network);
 const networkInfo = async () => {
    
     // Your code here!
-    let network = await hardhatProvider.getNetwork();
-    let name = network.name;
-    let chainId = network.chainId;
-    let blockN = await hardhatProvider.getBlockNumber();
-    console.log(name, chainId, blockN);
 };
 
-networkInfo();
+// networkInfo();
 
 
 // Exercise 3. Connect a signer to the Hardhat blockchain.
@@ -48,8 +43,7 @@ networkInfo();
 // Hint: you will find the info printed to console after you start the hardhat
 // blockchain.
 
-let hhPrivateKey = "0x8ecdb66d6ac608dd911cd5ac595e7f4c4a92a6f3bc3353076b00ce0291f21684";
-const signer = new ethers.Wallet(hhPrivateKey, hardhatProvider);
+let hhPrivateKey = "FILL_THIS_VALUE";
 
 // Your code here!
 
@@ -59,12 +53,9 @@ const signer = new ethers.Wallet(hhPrivateKey, hardhatProvider);
 const getNonce = async() => {
 
     // Your code here!
-    let nonce = await signer.getNonce();
-    console.log('Your nonce is ' + nonce);
-
 };
 
-getNonce();
+// getNonce();
 
 
 // Exercise 4. Check gas.
@@ -74,22 +65,14 @@ getNonce();
 // Check the slides in ILIAS.
 // Hint: only accessible within UniMa network.
 
-//how???
-// You are using the wrong chain! ??? maybe vpn?
-
 // b. Check your balance on UniMa network.
 
 const checkBalance = async () => {
 
     // Your code here!
-
-    let balance = await hardhatProvider.getBalance(signer.address);
-
-    console.log('My balance is ' + ethers.formatEther(balance) + ' ETH.');
-
 };
 
-checkBalance();
+// checkBalance();
 
 // Exercise 5. Send a transaction.
 //////////////////////////////////
@@ -101,34 +84,7 @@ const account2 = process.env.METAMASK_2_ADDRESS;
 const sendTransaction = async () => {
 
     // Your code here!
-
-    let b1 = await hardhatProvider.getBalance(signer.address);
-    let b2 = await hardhatProvider.getBalance(account2);
-    b1 = ethers.formatEther(b1);
-    b2 = ethers.formatEther(b2);
-    
-
-    tx = await signer.sendTransaction({
-        to: account2,
-        value: ethers.parseEther("0.01")
-    });
-
-    // console.log(tx);
-    
-    console.log('Transaction is in the mempool...');
-    await tx.wait();
-
-    console.log('Transaction mined!');
-
-    let updatedB1 = await hardhatProvider.getBalance(signer.address);
-    let updatedB2 = await hardhatProvider.getBalance(account2);
-    updatedB1 = ethers.formatEther(updatedB1);
-    updatedB2 = ethers.formatEther(updatedB2);
-
-    console.log('Balance for', signer.address, 'changed from', b1, 'to', updatedB1);
-    console.log('Balance for', account2, 'changed from', b2, 'to', updatedB2);
-
 };
 
-sendTransaction();
+// sendTransaction();
 
